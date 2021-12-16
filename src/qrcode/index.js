76bfce46b87587859df7b7cbb81fa847a0c7e893
Devin <chooin@126.com>
2021-12-16 09:04:53 +0800

    feat: base 64

--- src/qrcode/index.js
+++ src/qrcode/index.js
@@ -2,7 +2,7 @@ import alfy from "alfy";
 import queryString from "query-string";
 
 alfy.output(
-  [1200, 800, 600].map((size) => ({
+  [600, 800, 1200].map((size) => ({
     title: `${size}: ${alfy.input}`,
     subtitle: "Open in browser",
     arg: queryString.stringifyUrl({
